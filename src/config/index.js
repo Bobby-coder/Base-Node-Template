5c30b87732d4de04d5346fa8980440370739fe7a
Bobby-coder <bobbysadhwani612@gmail.com>
2023-05-19 14:16:56 +0530

    Routes added

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const dotenv = require("dotenv");
 
-// After calling config() method of dotenv object, all the .env variables are stored into the env object whhich is inside the global process object.process.env.VARIABLE_NAME
+// After calling config() method of dotenv object, all the .env variables are stored into the env object which is inside the global object "process" -> process.env.VARIABLE_NAME
 dotenv.config();
 
 // We will export all the env variables from this file and import wherever they are needed. For now we are exporting PORT variable.
--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ const express = require("express");
 // Here we are destructuring an object, we are extracting PORT property from the imported object using destructuring.
 const { PORT } = require("./config");
 
+const apiRoutes = require("./routes");
+
 const app = express();
+
+app.use("/api", apiRoutes);
 
 app.listen(PORT, () =>
   console.log(`Successfully started the server on PORT : ${PORT}`)
 );
-
