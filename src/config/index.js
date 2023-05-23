7430b972d7e7a7efac29a8f975bf8f7bf07d8b32
Bobby-coder <bobbysadhwani612@gmail.com>
2023-05-23 14:33:09 +0530

    Winston setup done

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,4 @@
-const dotenv = require("dotenv");
-
-// After calling config() method of dotenv object, all the .env variables are stored into the env object which is inside the global object "process" -> process.env.VARIABLE_NAME
-dotenv.config();
-
-// We will export all the env variables from this file and import wherever they are needed. For now we are exporting PORT variable.
 module.exports = {
-  PORT: process.env.PORT,
+  ServerConfig: require("./server-config"),
+  Logger: require("./logger-config"),
 };
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 // If we are importing from an index.js file, then we dont need to specifically mention /index at the path end. e.g, './config/index.js'
 // Here we are destructuring an object, we are extracting PORT property from the imported object using destructuring.
-const { PORT } = require("./config");
+const { ServerConfig, Logger } = require("./config");
 
 const apiRoutes = require("./routes");
 
@@ -10,6 +10,7 @@ const app = express();
 
 app.use("/api", apiRoutes);
 
-app.listen(PORT, () =>
-  console.log(`Successfully started the server on PORT : ${PORT}`)
-);
+app.listen(ServerConfig.PORT, () => {
+  console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
+  Logger.info("Successfully started the server", "root", {});
+});
