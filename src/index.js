52240de9bad0f52388ed64aa52ccc85544cbe1a4
Bobby-coder <bobbysadhwani612@gmail.com>
2023-05-25 12:18:05 +0530

    comment rectified

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
-// If we are importing from an index.js file, then we dont need to specifically mention /index at the path end. e.g, './config/index.js'
-// Here we are destructuring an object, we are extracting PORT property from the imported object using destructuring.
+// If we are importing from an index.js file, then we dont need to specifically mention "/index" at the path end. e.g, './config/index.js'
+// Here we are destructuring an object, we are extracting ServerConfig & Logger object from the imported object using destructuring.
 const { ServerConfig, Logger } = require("./config");
 
 const apiRoutes = require("./routes");
